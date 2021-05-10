d7923054d2b15691cd95f503095f6f49faf1b78f
matheusmp852 <gota852@gmail.com>
2021-05-10 10:32:24 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Controllers/ContaController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Teste.Models;
+
+namespace Teste.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var achou = (login.Usuario == "matheus" && login.Senha == "123");
+
+            if (achou)
+            {
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.lembrarMe);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login inválido.");
+            }
+
+
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
---
+++ Teste/Models/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Teste.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o usuario")]
+        [Display(Name = "Usuário: ")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display (Name = "Senha: ")]
+        public string Senha { get; set; }
+       
+        [Display(Name = "Lembrar Me")]
+
+        public bool lembrarMe { get; set; }
+    }
+}
